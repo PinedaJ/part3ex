8a0de0ef4448231ca9918d5bfaa2b7d9e9cbe379
PinedaJ <jeff.c.pineda@gmail.com>
2021-08-23 13:51:47 -0400

    setup for create heroku

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
-
 app.use(morgan('tiny'))
 
 let persons = [
@@ -108,7 +109,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
